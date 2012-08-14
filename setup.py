96c2859cfcd6720e5f4f5db3146bf0bde5dd1b7b
quasinerd <vgluschuk@gmail.com>
2012-08-14 11:02:38 +0300

    Added yammy2html command-line utility.
    --HG--
    extra : rebase_source : 381b2bc26c0c62de5ed0a09925b81c7f8bd7fc30

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '0.3'
 
 setup(name='yammy',
       version=version,
--- yammy/jinja2_loaders.py
+++ yammy/jinja2_loaders.py
@@ -4,9 +4,9 @@ from yammy.translator import yammy_to_html_string
 
 
 class YammyLoaderMixin(object):
-    
+
     def get_html_source(self, get_source, environment, template):
-        contents, filename, uptodate = get_source(environment, template) 
+        contents, filename, uptodate = get_source(environment, template)
         if filename.endswith('.ymy') or filename.endswith('.yammy'):
             contents = yammy_to_html_string(contents)
         return contents, filename, uptodate
@@ -15,10 +15,12 @@ class YammyLoaderMixin(object):
 class YammyFileSystemLoader(FileSystemLoader, YammyLoaderMixin):
 
     def get_source(self, environment, template):
-        return self.get_html_source(super(YammyFileSystemLoader, self).get_source, environment, template)
+        source = super(YammyFileSystemLoader, self).get_source
+        return self.get_html_source(source, environment, template)
 
 
 class YammyPackageLoader(PackageLoader, YammyLoaderMixin):
 
     def get_source(self, environment, template):
-        return self.get_html_source(super(YammyPackageLoader, self).get_source, environment, template)
+        source = super(YammyPackageLoader, self).get_source
+        return self.get_html_source(source, environment, template)
